ef191413510d6323c618a9d8d62dafd106086667
mrbernnz <noel.sagaille@gmail.com>
2017-04-18 18:23:13 -0400

    Refactor API calls.
    Why:
    
      The Axios library has built in promises.
    
    Changes:
    
      [√] :wrench: .env.sample
      [√] :wrench: app.js
      [√] :wrench: package.json
      [√] :fire:  geocode.js
      [√] :fire:  weather.js
      [√] :wrench: yarn.lock
    
    Notes:
    
      None.

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-const request = require('request')
-const { geocode } = require('./geocode')
-const { weather } = require('./weather')
+const axios = require('axios')
+const encodedAddress = encodeURIComponent(11203)
+const url = process.env.GOOGLE_MAPS_API + encodedAddress
 
-const requestedZip = 11203
-geocode(requestedZip, (errorMessage, coordinates) => {
-  if (errorMessage) {
-    throw errorMessage
-  } else {
-    weather(coordinates, (error, { temperature, apparentTemperature }) => {
-      console.log(temperature)
-      console.log(apparentTemperature)
-    })
-  }
-})
+axios
+  .get(url)
+  .then(({ data }) => {
+    if (data.status === 'ZERO_RESULTS') {
+      throw new Error('CANNOT TO FIND ADDRESS')
+    }
+
+    let lat = data.results[0].geometry.location.lat
+    let lng = data.results[0].geometry.location.lng
+    const weatherURL = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${lat},${lng}`
+    return axios.get(weatherURL)
+  })
+  .then(({ data }) => {
+    return {
+      temperature: data.currently.temperature,
+      apparentTemperature: data.currently.apparentTemperature
+    }
+  })
+  .catch(e => {
+    if (e.code === 'ENOTFOUND') {
+      console.log('Unable to connect to API server.')
+      // throw new Error('Unable to connect to API server.')
+    } else if (e.code === 'ECONNREFUSED') {
+      console.log('Connection is refused.')
+      // throw new Error('Connection is refused.')
+    } else {
+      console.log(e.message)
+      // throw new Error(e.message)
+    }
+  })
