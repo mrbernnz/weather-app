b47aeba866e28491591ae41c141f281038858d61
mrbernnz <noel.sagaille@gmail.com>
2017-05-12 12:17:44 -0400

    * Create an express app.
      Why:
    
        Will use this as the resource to use for weather example under
        Snuggsi
    
      Changes:
    
        [√] :wrench: app.js
        [√] :page_facing_up: index.html
        [√] :wrench: package.json
        [√] :wrench: yarn.lock

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
+const app = require('express')()
 const axios = require('axios')
 const encodedAddress = encodeURIComponent(11203)
 const url = process.env.GOOGLE_MAPS_API + encodedAddress
 
-axios
-  .get(url)
-  .then(({ data }) => {
-    if (data.status === 'ZERO_RESULTS') {
-      throw new Error('CANNOT TO FIND ADDRESS')
-    }
 
-    let lat = data.results[0].geometry.location.lat
-    let lng = data.results[0].geometry.location.lng
-    const weatherURL = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${lat},${lng}`
-    return axios.get(weatherURL)
+app
+  .set('port', 4000 || process.env.PORT)
+  .get('/', (req, res) => {
+    axios
+      .get(url)
+      .then(({ data }) => {
+        if (data.status === 'ZERO_RESULTS') {
+          throw new Error('CANNOT TO FIND ADDRESS')
+        }
+
+        let lat = data.results[0].geometry.location.lat
+        let lng = data.results[0].geometry.location.lng
+        const weatherURL = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${lat},${lng}`
+        return axios.get(weatherURL)
+      })
+      .then(({ data }) => {
+        res.send(`<h1>${data.currently.temperature}</h1>`)
+      })
+      .catch(e => {
+        if (e.code === 'ENOTFOUND') {
+          console.log('Unable to connect to API server.')
+          // throw new Error('Unable to connect to API server.')
+        } else if (e.code === 'ECONNREFUSED') {
+          console.log('Connection is refused.')
+          // throw new Error('Connection is refused.')
+        } else {
+          console.log(e.message)
+          // throw new Error(e.message)
+        }
+      })
   })
-  .then(({ data }) => {
-    return {
-      temperature: data.currently.temperature,
-      apparentTemperature: data.currently.apparentTemperature
-    }
-  })
-  .catch(e => {
-    if (e.code === 'ENOTFOUND') {
-      console.log('Unable to connect to API server.')
-      // throw new Error('Unable to connect to API server.')
-    } else if (e.code === 'ECONNREFUSED') {
-      console.log('Connection is refused.')
-      // throw new Error('Connection is refused.')
-    } else {
-      console.log(e.message)
-      // throw new Error(e.message)
-    }
-  })
+
+  .listen(app.get('port'))
